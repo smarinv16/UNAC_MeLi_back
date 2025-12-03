19671b06af4bc0bd1ebf1b3dc41aff35f6c748be
smarinv16 <smarinviana@gmail.com>
2025-12-03 07:21:50 -0500

    Origanizacion y modificacion a credenciales para conexion a db

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 
 
 const sequelize = new Sequelize (
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
+    process.env.DB_NAME || process.env.MYSQL_DATABASE_UNAC,
+    process.env.DB_USER || process.env.MYSQLUSER,
+    process.env.DB_PASSWORD || process.env.MYSQLPASSWORD, 
+    {
+        host: process.env.DB_HOST || process.env.MYSQLHOST,
         dialect: 'mysql',
-        port : process.env.DB_PORT,
+        port : process.env.DB_PORT || process.env.MYSQLPORT,
         logging : false,
     }
 );
